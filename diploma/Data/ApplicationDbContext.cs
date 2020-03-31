b8c07c3d6e0dca2f9915d43af4bed0a5b37f65ac
Alexeyyy <a.zhelepov@yandex.ru>
2020-03-31 07:10:25 +0400

    Initial commit

---
+++ diploma/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using diploma.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace diploma.Data
+{
+    // https://www.alfador.mx/software-development/configure-aspnet-core-2-mvc-identity-to-use-postgresql .
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ diploma/Data/Entities/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace diploma.Data.Entities
+{
+    public class UserInfo : IdentityUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CareerStart { get; set; }
+        public DateTime? VacationStart { get; set; }
+    }
+}
